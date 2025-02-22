a53c6ea11a62bc24307e51f227809980e7bf040f
amehanthony <amehanthonyowoi@gmail.com>
2025-02-22 19:54:30 +0100

    feat: Ensure each bar in the plot has a different color using seaborn color palette

--- tracker/visualizer.py
+++ tracker/visualizer.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Visualizer:
     def plot(self, data):
-        data.groupby("activity").sum().plot(kind="bar")
+        colors = sns.color_palette("husl", len(data["activity"].unique()))
+        activity_data = data.groupby("activity").sum()
+        
+        # Plot each bar with a different color
+        ax = activity_data.plot(kind="bar", legend=False)
+        for i, bar in enumerate(ax.patches):
+            bar.set_color(colors[i % len(colors)])
+        
+        plt.xlabel("Activity")
+        plt.ylabel("Total Duration (hours)")
+        plt.title("Time Spent on Different Activities")
         plt.show()
