1ad10b302998c5d36103cde75401f628eb3dedc0
amehanthony <amehanthonyowoi@gmail.com>
2025-03-02 23:46:34 +0100

    update unit tests for tracker class

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,11 +1,28 @@
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from tracker.tracker import Tracker
 
 class TestTracker(unittest.TestCase):
+    def setUp(self):
+        self.tracker = Tracker()
+
+    def test_initial_data(self):
+        self.assertTrue(self.tracker.data.empty)
+        self.assertEqual(self.tracker.activities, [])
+
     def test_add_hit(self):
-        tracker = Tracker()
-        tracker.add_hit("coding", 2)
-        self.assertEqual(len(tracker.get_data()), 1)
+        self.tracker.add_hit("Coding", 2)
+        self.assertEqual(len(self.tracker.data), 1)
+        self.assertEqual(self.tracker.data.iloc[0]["activity"], "Coding")
+        self.assertEqual(self.tracker.data.iloc[0]["duration"], 2)
+
+    def test_track_activity(self):
+        self.tracker.track_activity("Coding")
+        self.assertEqual(self.tracker.activities, ["Coding"])
 
 if __name__ == "__main__":
     unittest.main()
